026e33374f1894c3562d89718b97f45107ce4741
Kirill Baranov <K.Baranov@aladdin.ru>
2022-07-23 15:02:29 +0300

    Движение армии вторжения вправо-влево

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -30,6 +30,7 @@ class AlienInvasion:
         '''Запуск основного цикла игры'''
         while True:
             self._check_events()
+            self._update_aliens()
             self._update_screen()
             self.ship.update()
             self._update_bullets()
@@ -78,22 +79,33 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        #print(len(self.bullets))
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Обработка коллизий снарядов с пришельцами"""
+        # При обнаружении попадания удалить снаряд и пришельца
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # уничтожение существующих снаряжов и создание нового флота
+            self.bullets.empty()
+            self._create_fleet()
 
     def _create_fleet(self):
         """Создание флота вторжения"""
-        # Создание пришельца
+        # Создание пришельца и вычисление количества пришельцев в ряду
+        # Интервал между соседними пришельцами равен ширине пришельца
         alien = Alien(self)
         alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
-        number_aliens_x = available_space_x
+        number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Определяет количество рядов, помещающихся на экране
+        #Определяет количество рядов, помещающихся на экране
         ship_height = self.ship.rect.height
         available_space_y = (self.settings.screen_height -
                              (3 * alien_height) - ship_height)
         number_rows = available_space_y // (2 * alien_height)
-
         # Создание флота вторжения
         for row_number in range(number_rows):
             for alien_number in range(number_aliens_x):
@@ -107,6 +119,24 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """ Опускает весь флот и меняет направление флота"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        """ Обновляет расположение пришельца"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _update_screen(self):
         # При каждом проходе цикла перерисовывается экран
         self.screen.fill(self.settings.bg_color)
@@ -117,6 +147,7 @@ class AlienInvasion:
 
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
+
 
 
 if __name__ == '__main__':
