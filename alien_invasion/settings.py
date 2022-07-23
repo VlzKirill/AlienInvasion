7e90b88d6c2ee46aa73b38cc36347fa2c25d68de
Kirill Baranov <K.Baranov@aladdin.ru>
2022-07-23 12:13:10 +0300

    создание армии (в несколько рядов)

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,16 +1,16 @@
-class Settings():
+class Settings:
     """ Класс для хранения всех настроек игры Alien Invasion"""
 
     def __init__(self):
         """Инициализирует настройки игры"""
-        #Параметры экрана
+        # Параметры экрана
         self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
         self.sheep_speed = 0.5
 
-        #Параметры снаряда (пули)
+        # Параметры снаряда (пули)
         self.bullet_speed = 1
         self.bullet_width = 3
         self.bullet_height = 15
